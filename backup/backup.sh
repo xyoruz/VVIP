845ad785e48e11c628f95f5e5ab1e44f823945de
XYR <169094507+xyoruz@users.noreply.github.com>
2024-05-18 06:44:09 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -131,15 +131,15 @@ mv /root/$NameUser.zip /root/user-backup/$NameUser/
 Save_And_Exit () {
     DATE=$(date +'%d %B %Y')
     cd /root/user-backup
-    git config --global user.email "alnurridwan112@gmail.com" &> /dev/null
-    git config --global user.name "myridwan" &> /dev/null
+    git config --global user.email "xyr3760@gmail.com" &> /dev/null
+    git config --global user.name "xyoruz" &> /dev/null
     rm -rf .git &> /dev/null
     git init &> /dev/null
     git add . &> /dev/null
     git commit -m backup &> /dev/null
     git branch -M ipuk &> /dev/null
-    git remote add origin https://github.com/myridwan/backup
-    git push -f https://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@github.com/myridwan/backup.git &> /dev/null
+    git remote add origin https://github.com/xyoruz/backup
+    git push -f https://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@github.com/xyoruz/backup.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
