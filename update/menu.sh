d12f2107a3e6523bdef64331c647761604a02898
XYR <169094507+xyoruz@users.noreply.github.com>
2024-05-18 04:40:21 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # =========================================
-BOT="https://raw.githubusercontent.com/xyoruz/src/ipuk/"
+BOT="https://raw.githubusercontent.com/xyoruz/scriptvpn/main/bot.sh"
 UPDATE="https://raw.githubusercontent.com/xyoruz/VVIP/ipuk/update/update.sh"
 GREEN='\033[0;32m'
 RED='\033[0;31m'
